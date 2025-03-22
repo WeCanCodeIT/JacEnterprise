0ef004c8120f96712a84509aed227bbcda36bb9e
JohnWCCI <123778697+JohnWCCI@users.noreply.github.com>
2025-03-22 11:23:08 -0400

    initial

---
+++ Backend/jac_api/src/main/java/org/wcci/jac_api/JacApiApplication.java
@@ -0,0 +1,13 @@
+package org.wcci.jac_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacApiApplication.class, args);
+	}
+
+}
